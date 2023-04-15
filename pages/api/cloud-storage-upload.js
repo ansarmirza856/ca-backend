9a2d3333401d5847bdfd5fc63725fdcd01b2df0f
ansarmirza856 <128377922+ansarmirza856@users.noreply.github.com>
2023-04-15 09:18:38 +0200

    Update cloud-storage-upload.js

--- pages/api/cloud-storage-upload.js
+++ pages/api/cloud-storage-upload.js
@@ -29,23 +29,45 @@ export default async function handler(req, res) {
       const action = req.body.action;
 
       for (let i = 0; i < req.files.length; i++) {
-        const fileName =
-          uuidv4().slice(0, 10).split("-").join("") +
-          "-" +
-          req.files[i].originalname;
+        
+        
+        
 
         const s3 = new AWS.S3({
           region: process.env.CLOUD_REGION,
           accessKeyId: process.env.CLOUD_ACCESS_KEY_ID,
           secretAccessKey: process.env.CLOUD_ACCESS_KEY_SECRET,
         });
-
+        
+        
+         if(action === "delivery-files"){
+           
+           const fileName =
+          uuidv4().slice(0, 10).split("-").join("") +
+          "-" +
+          req.files[i].originalname;
+           
         const params = {
           Bucket: process.env.CLOUD_BUCKET_NAME,
           Key: fileName,
           Body: req.files[i].buffer,
           ContentType: req.files[i].mimetype,
         };
+         }else if(action === "user-files"){
+           
+           const fileName =
+          uuidv4().slice(0, 10).split("-").join("") +
+          "-" +
+          req.files[i].fileName;
+           
+         const params = {
+          Bucket: process.env.CLOUD_BUCKET_NAME,
+          Key: fileName,
+          Body: req.files[i].uri,
+          ContentType: req.files[i].type,
+        };
+         
+         }
 
         s3.putObject(params).promise();
 
@@ -71,8 +93,6 @@ export default async function handler(req, res) {
             .json({ success: false, error: "No form found" });
         }
       } else if (action === "user-files") {
-        
-        return res.status(200).json({formId: formId, action: action, files: req.files  });
         
         const userTaxApplication = await UserTaxApplication.findOneAndUpdate(
           { formId: formId },
