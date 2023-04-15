0c4acf54f16aecbe58d6376e107ec866a3fdce49
ansarmirza856 <128377922+ansarmirza856@users.noreply.github.com>
2023-04-15 04:04:01 +0200

    Update verify-otp.js

--- pages/api/verify-otp.js
+++ pages/api/verify-otp.js
@@ -15,45 +15,45 @@ export default async function handler(req, res) {
       const user = await User.findOne({ email });
 
       if (!user) {
-        return res.status(400).json({ message: `User not found. ${email}` });
+        return res.status(400).json({success: false, message: `User not found. ${email}` });
       }
 
       if (action === "email-verification") {
         if (user.isEmailVerified) {
-          return res.status(400).json({ message: "Email already verified" });
+          return res.status(400).json({success: false, message: "Email already verified" });
         }
 
         if (user.otp !== otp) {
-          return res.status(400).json({ message: "Invalid OTP code" });
+          return res.status(400).json({success: false, message: "Invalid OTP code" });
         }
 
         user.isEmailVerified = true;
         user.otp = "";
         await user.save();
 
-        return res.status(200).json({ message: "OTP verified successfully" });
+        return res.status(200).json({success: true, message: "OTP verified successfully" });
       } else if (action === "reset-password") {
         if (user.resetPasswordToken !== otp) {
-          return res.status(400).json({ message: "Invalid OTP code" });
+          return res.status(400).json({success: false, message: "Invalid OTP code" });
         }
 
         if (Date.now() > user.resetPasswordExpires) {
-          return res.status(400).json({ message: "OTP expired" });
+          return res.status(400).json({success: false, message: "OTP expired" });
         }
 
         user.resetPasswordToken = "";
         user.resetPasswordExpires = "";
         await user.save();
 
-        return res.status(200).json({ message: "OTP verified successfully" });
+        return res.status(200).json({success: true, message: "OTP verified successfully" });
       } else {
-        return res.status(400).json({ message: "Invalid request" });
+        return res.status(400).json({success: false, message: "Invalid request" });
       }
     } catch (error) {
       console.error(error);
-      return res.status(500).json({ message: "Internal server error" });
+      return res.status(500).json({success: false, message: "Internal server error" });
     }
   } else {
-    return res.status(400).json({ message: "Invalid request" });
+    return res.status(400).json({success: false, message: "Invalid request" });
   }
 }
