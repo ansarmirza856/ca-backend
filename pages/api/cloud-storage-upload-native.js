9363c3f0a6fc574886b25fa0852a01a0a44bb53d
ansarmirza856 <128377922+ansarmirza856@users.noreply.github.com>
2023-04-16 13:27:34 +0200

    Add files via upload

--- pages/api/cloud-storage-upload-native.js
+++ pages/api/cloud-storage-upload-native.js
@@ -24,9 +24,9 @@ export default async function handler(req, res) {
       //   return res.status(400).json({ error: "Failed to upload file" });
       // }
 
-      return res
-        .status(200)
-        .json({ success: true, body: req.body, file: req.file });
+      // return res
+      //   .status(200)
+      //   .json({ success: true, body: req.body, file: req.file });
 
       const uploadedFiles = [];
       const formId = req.body.formId;
