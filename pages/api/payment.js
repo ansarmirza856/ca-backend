1c46908104e49a87713dc0b88616300625b149db
ansarmirza856 <128377922+ansarmirza856@users.noreply.github.com>
2023-04-17 12:17:35 +0200

    Add files via upload

--- pages/api/payment.js
+++ pages/api/payment.js
@@ -9,18 +9,43 @@ export default authMiddleware(async function handler(req, res) {
     //add stripe
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
+    await connectDB();
+
+    if (req.body === null) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Payload should not be empty" });
+    }
+
+    const getUserApplication = await userTaxApplication.findOne({
+      formId: req.body.formId,
+    });
+
+    if (!getUserApplication) {
+      return res.status(404).json({ success: false, error: "No form found" });
+    }
+
+    const { formId, accountancyFee, firstName, lastName, userEmail } =
+      getUserApplication;
+
     try {
       //create payment intent
       const paymentIntent = await stripe.paymentIntents.create({
-        amount: req.body.amount,
+        amount: accountancyFee * 100,
         currency: "gbp",
         payment_method_types: ["card"],
-        receipt_email: req.user.email,
+        receipt_email: userEmail,
       });
 
-      res
-        .status(200)
-        .json({ success: true, paymentIntent: paymentIntent.client_secret });
+      res.status(200).json({
+        success: true,
+        paymentIntent: paymentIntent.client_secret,
+        formId: formId,
+        accountancyFee: accountancyFee,
+        firstName: firstName,
+        lastName: lastName,
+        userEmail: userEmail,
+      });
 
       //save payment intent id to db
       //   const savedSelfEmployments = await userTaxApplication.findOneAndUpdate(
@@ -30,7 +55,9 @@ export default authMiddleware(async function handler(req, res) {
       //   );
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: "Internal server error", log: error });
+      res
+        .status(500)
+        .json({ success: false, error: "Internal server error", log: error });
     }
   } else {
     res.status(400).json({ success: false });
