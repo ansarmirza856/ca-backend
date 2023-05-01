c356176c3b96b190e3c41f372f0aa6c6cb628207
ansarmirza856 <128377922+ansarmirza856@users.noreply.github.com>
2023-05-01 15:51:56 +0200

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -9,6 +9,9 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  surName: {
+    type: String,
+  },
   email: {
     type: String,
     required: true,
@@ -42,6 +45,16 @@ const userSchema = new mongoose.Schema({
     type: Date,
   },
   isAdmin: { type: Boolean, default: false },
+  uTrNumber: { type: Number, required: true },
+  nInumber: { type: String, required: true },
+  dateOfBirth: { type: Date, required: true },
+  currentAddress: {
+    addressLine1: { type: String },
+    addressLine2: { type: String },
+    town: { type: String },
+    postCode: { type: String },
+    county: { type: String },
+  },
   timestamp: { type: Date, default: Date.now },
 });
 
