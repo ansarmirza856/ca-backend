f26e006ef1b5337dd1a8e0c0e7cd174cd90401c9
ansarmirza856 <128377922+ansarmirza856@users.noreply.github.com>
2023-06-01 22:45:14 +0200

    Add files via upload

---
+++ utils/utils.js
@@ -0,0 +1,28 @@
+var SibApiV3Sdk = require("sib-api-v3-sdk");
+
+export const sendEmail = (email, subject, body) => {
+  // Sendinblue email API integration
+  const defaultClient = SibApiV3Sdk.ApiClient.instance;
+  const apiKey = defaultClient.authentications["api-key"];
+  apiKey.apiKey = process.env.SENDINBLUE_API_KEY;
+  const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+  const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+  sendSmtpEmail.subject = subject;
+  sendSmtpEmail.htmlContent = `${body}`;
+  sendSmtpEmail.sender = {
+    name: "Crawley Accounts",
+    email: "noreply@caaccounts.com",
+  };
+  sendSmtpEmail.to = [{ email }];
+  sendSmtpEmail.replyTo = { email: "noreply@myapp.com" };
+
+  // Send the email
+  apiInstance.sendTransacEmail(sendSmtpEmail).then(
+    function (data) {
+      console.log("Email sent successfully");
+    },
+    function (error) {
+      console.error("Error sending email: ", error);
+    }
+  );
+};
