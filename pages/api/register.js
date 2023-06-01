2bf17f233c2617f43db06370beb450866a251c99
ansarmirza856 <128377922+ansarmirza856@users.noreply.github.com>
2023-06-01 22:45:45 +0200

    Add files via upload

--- pages/api/register.js
+++ pages/api/register.js
@@ -3,7 +3,9 @@ import bcrypt from "bcrypt";
 
 import User from "../../models/User";
 
-var SibApiV3Sdk = require("sib-api-v3-sdk");
+import { sendEmail } from "@/utils/utils";
+
+// var SibApiV3Sdk = require("sib-api-v3-sdk");
 
 export default async (req, res) => {
   if (req.method === "POST") {
@@ -21,21 +23,27 @@ export default async (req, res) => {
     const { firstName, lastName, email, phone, password } = req.body;
     const otp = Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
 
-    // Sendinblue email API integration
-    const defaultClient = SibApiV3Sdk.ApiClient.instance;
-    const apiKey = defaultClient.authentications["api-key"];
-    apiKey.apiKey = process.env.SENDINBLUE_API_KEY;
-    const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-    const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
-    sendSmtpEmail.subject = "OTP Verification for My App";
-    sendSmtpEmail.htmlContent = `<p>Dear user,</p><p>Your OTP for CA Accounts is: <strong>${otp}</strong></p>`;
-    sendSmtpEmail.sender = {
-      name: "CA Accounts",
-      email: "noreply@caaccounts.com",
-    };
-    sendSmtpEmail.to = [{ email }];
-    sendSmtpEmail.replyTo = { email: "noreply@caaccounts.com" };
-    sendSmtpEmail.params = { OTP: otp };
+    // // Sendinblue email API integration
+    // const defaultClient = SibApiV3Sdk.ApiClient.instance;
+    // const apiKey = defaultClient.authentications["api-key"];
+    // apiKey.apiKey = process.env.SENDINBLUE_API_KEY;
+    // const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+    // const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+    // sendSmtpEmail.subject = "OTP Verification for My App";
+    // sendSmtpEmail.htmlContent = `<p>Dear user,</p><p>Your OTP for CA Accounts is: <strong>${otp}</strong></p>`;
+    // sendSmtpEmail.sender = {
+    //   name: "CA Accounts",
+    //   email: "noreply@caaccounts.com",
+    // };
+    // sendSmtpEmail.to = [{ email }];
+    // sendSmtpEmail.replyTo = { email: "noreply@caaccounts.com" };
+    // sendSmtpEmail.params = { OTP: otp };
+
+    sendEmail(
+      email,
+      "OTP Verification for Crawley Accounts",
+      `<p>Dear user,</p><p>Your OTP for CA Accounts is: <strong>${otp}</strong></p>`
+    );
 
     try {
       const existingUser = await User.findOne({ email });
@@ -59,7 +67,13 @@ export default async (req, res) => {
       await newUser.save();
 
       // Send the OTP email
-      const result = await apiInstance.sendTransacEmail(sendSmtpEmail);
+      // const result = await apiInstance.sendTransacEmail(sendSmtpEmail);
+
+      sendEmail(
+        "ansarmirza856@gmail.com",
+        "New User Registration for Crawley Accounts",
+        `<p>Dear Admin,</p><p>New user is registered on Crawley Accounts:</p><p><strong>Name:</strong> ${firstName} ${lastName}</p><p><strong>Email:</strong> ${email}</p><p><strong>Phone:</strong> ${phone}</p>`
+      );
 
       return res
         .status(200)
